640b27a62e45de764902bc8a4bf6c5b4761eb8d9
jaswanth264 <jchallag@gitam.in>
2025-07-23 15:52:38 +0530

    Update superClient.js

--- superClient.js
+++ superClient.js
@@ -1,10 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 07fa4f27cec295dad4dbdb457bb3806433f3a5ac
